ee5ebf82b357e7a9e328a5eb81d9ef0f6a589142
Денис Степаненко <denis.stepanenko@hotmail.com>
2022-08-11 09:41:45 +0300

    Add project files.

---
+++ Task4/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace Task4
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Введите первое число");
+            int a = int.Parse(Console.ReadLine());
+
+            Console.WriteLine("Введите второе число");
+            int b = int.Parse(Console.ReadLine());
+
+            Console.WriteLine("Введите третье число");
+            int c = int.Parse(Console.ReadLine());
+
+            Console.WriteLine("Результат");
+            int max = a;
+            if (max < b) max = b;
+            if (max < c) max = c;
+            Console.WriteLine(max);
+            Console.ReadLine();
+        }
+    }
+}
